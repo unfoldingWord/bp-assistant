3f2aba8de9c2be48128d0f6976e59e14d237098a
stephenwunrow <stephen.wunrow@my.wheaton.edu>
2026-06-02 14:50:22 -0500

    adding replacements for hebrew path

--- src/fill_quotes.py
+++ src/fill_quotes.py
@@ -784,12 +784,14 @@ def update_json_from_tsv(json_file, origl_and_snippet):
                 item["orig_quote"] = english_phrase
                 item["gl_quote"] = english_phrase
                 item["issue_span_gl_quote"] = english_phrase
-
+                item["exact_ult_span"] = english_phrase
+                
                 # Nested writer_packet replacements
                 if "writer_packet" in item:
                     item["writer_packet"]["orig_quote"] = english_phrase
                     item["writer_packet"]["gl_quote"] = english_phrase
                     item["writer_packet"]["issue_span_gl_quote"] = english_phrase
+                    item["writer_packet"]["exact_ult_span"] = english_phrase
 
     def safe_write_json(json_file, data):
         # 1. Write to temp file in same directory
