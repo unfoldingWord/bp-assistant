7e9b68e1556a92f8ca4441e71230bac51891dd90
stephenwunrow <stephen.wunrow@my.wheaton.edu>
2026-06-02 14:40:51 -0500

    replacing exact_ult_span for AT generation

--- src/fill_quotes.py
+++ src/fill_quotes.py
@@ -766,12 +766,14 @@ def update_json_from_tsv(json_file, origl_and_snippet):
             item["orig_quote"] = hebrew_phrase
             item["gl_quote"] = english_phrase
             item["issue_span_gl_quote"] = english_phrase
+            item["exact_ult_span"] = english_phrase
 
             # Nested writer_packet replacements
             if "writer_packet" in item:
                 item["writer_packet"]["orig_quote"] = hebrew_phrase
                 item["writer_packet"]["gl_quote"] = english_phrase
                 item["writer_packet"]["issue_span_gl_quote"] = english_phrase
+                item["exact_ult_span"] = english_phrase
         
         if hebrew_phrase == '':
             if english_phrase == '':
