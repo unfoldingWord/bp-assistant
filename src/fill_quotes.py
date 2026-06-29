cb4adedebfb51bb6499653b80937e2960ebbb0c7
stephenwunrow <stephen.wunrow@my.wheaton.edu>
2026-06-29 15:49:38 -0500

    remove squiggly brackets from phrases in prepared_notes.json

--- src/fill_quotes.py
+++ src/fill_quotes.py
@@ -763,6 +763,7 @@ def update_json_from_tsv(json_file, origl_and_snippet):
 
         hebrew_phrase = update["hebrew_phrase"]
         english_phrase = update["english_phrase"]
+        english_phrase = re.sub(r'[}{]', '', english_phrase)
         english_phrase = re.sub(r'[“”]', '"', english_phrase)
         english_phrase = re.sub(r'[‘’]', "'", english_phrase)
         if english_phrase.count('"') == 1 and (english_phrase.startswith('"') or english_phrase.endswith('"')):
